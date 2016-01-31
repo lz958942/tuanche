b54c5ab50840c41a8992b985336b364cc671c4fb
country <country3721@163.com>
2016-01-31 14:58:23 +0800

    地图Util-添加代码实体

--- square-common/src/main/java/com/tuanche/common/util/MapUtil.java
+++ square-common/src/main/java/com/tuanche/common/util/MapUtil.java
@@ -4,4 +4,74 @@ package com.tuanche.common.util;
  * Created by tc on 2016/1/31.
  */
 public class MapUtil {
+
+    static double DEF_PI = 3.14159265359; // PI
+    static double DEF_2PI= 6.28318530712; // 2*PI
+    static double DEF_PI180= 0.01745329252; // PI/180.0
+    static double DEF_R =6370693.5; // 地球半径
+
+
+    /**
+     * 获得两点之间距离(近)
+     * @param lon1 A点经度
+     * @param lat1 A点纬度
+     * @param lon2 B点经度
+     * @param lat2 B点纬度
+     * @return 两点之间距离(米)
+     */
+    public static double GetShortDistance(double lon1, double lat1, double lon2, double lat2)
+    {
+        double ew1, ns1, ew2, ns2;
+        double dx, dy, dew;
+        double distance;
+        // 已经纬度为标准单位,映射到半个球面上
+        //通过计算在球面上的距离差
+        ew1 = lon1 * DEF_PI180;
+        ns1 = lat1 * DEF_PI180;
+        ew2 = lon2 * DEF_PI180;
+        ns2 = lat2 * DEF_PI180;
+        // 经度差
+        dew = ew1 - ew2;
+        // 若跨东经和西经180 度，进行调整
+        if (dew > DEF_PI)
+            dew = DEF_2PI - dew;
+        else if (dew < -DEF_PI)
+            dew = DEF_2PI + dew;
+        dx = DEF_R * Math.cos(ns1) * dew; // 东西方向长度(在纬度圈上的投影长度)
+        dy = DEF_R * (ns1 - ns2); // 南北方向长度(在经度圈上的投影长度)
+        // 勾股定理求斜边长
+        distance = Math.sqrt(dx * dx + dy * dy);
+        return distance;
+    }
+
+
+    /**
+     * 获得两点之间距离(长)
+     * @param lon1 A点经度
+     * @param lat1 A点纬度
+     * @param lon2 B点经度
+     * @param lat2 B点纬度
+     * @return 两点之间距离(米)
+     */
+    public double getLongDistance(double lon1, double lat1, double lon2,
+                                  double lat2) {
+        double ew1, ns1, ew2, ns2;
+        double distance;
+        // 角度转换为弧度
+        ew1 = lon1 * DEF_PI180;
+        ns1 = lat1 * DEF_PI180;
+        ew2 = lon2 * DEF_PI180;
+        ns2 = lat2 * DEF_PI180;
+        // 求大圆劣弧与球心所夹的角(弧度)
+        distance = Math.sin(ns1) * Math.sin(ns2) + Math.cos(ns1)
+                * Math.cos(ns2) * Math.cos(ew1 - ew2);
+        // 调整到[-1..1]范围内，避免溢出
+        if (distance > 1.0)
+            distance = 1.0;
+        else if (distance < -1.0)
+            distance = -1.0;
+        // 求大圆劣弧长度
+        distance = DEF_R * Math.acos(distance);
+        return distance;
+    }
 }
